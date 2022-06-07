d23a3a7443f3614736e2685043c468dc78f94222
Zera <51274450+ZeraTS@users.noreply.github.com>
2022-06-06 23:10:37 -0400

    REL 1.0.0
    Much of the code is old so there isn't much organization going around. Hopefully can change this on 1.1.0 and fix up the old broken tabstyle lol. You must install poppins + verminvibes font to use this.

---
+++ Rogue/ApplyChanges.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Rogue
+{
+    /// <summary>
+    /// Interaction logic for ApplyChanges.xaml
+    /// </summary>
+    public partial class ApplyChanges : Window
+    {
+        public ApplyChanges()
+        {
+            InitializeComponent();
+        }
+
+        private void next_Copy2_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void next_Copy1_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Rogue/Executor.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Xml;
+
+namespace Rogue
+{
+    /// <summary>
+    /// Interaction logic for Executor.xaml
+    /// </summary>
+    public partial class Executor : Window
+    {
+        
+        public Executor()
+        {
+            InitializeComponent();
+
+
+        }
+
+    }
+}
